245ed4465a9d9db4e7b52c9b38d75c52b9c3c922
shifttogit-deepanshu <sharmadeepanshu.kaizen1@gmail.com>
2022-01-30 17:47:39 +0530

    added import statements

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from distutils.log import debug
-from flask import Flask
+import numpy as np
+import os
+import cv2
 import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+
+# Flask utils
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-
-
-@app.route("/")
+@app.route('/', methods=['GET'])
 def index():
-    return "Hello World!dfgdfgf"
+    # Main page
+    return "Hello World Let's see!!!!!"
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
